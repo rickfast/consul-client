170bbafcdc7dbf9fd118ca20fef3fd7e9ccc6a40
Lais Andrade <lais.andrade@liferay.com>
2016-05-31 10:48:14 -0700

    Adds HealthCheckCache for health api /v1/health/state/<state> (#134)
    * Adds method to HealthClient for asynchronous call to health checks by state rest endpoint.
    
    * Adds HealthCheckCache implementation of ConsulCache.

---
+++ src/main/java/com/orbitz/consul/cache/HealthCheckCache.java
@@ -0,0 +1,56 @@
+package com.orbitz.consul.cache;
+
+import com.google.common.base.Function;
+import com.google.common.net.HostAndPort;
+import com.orbitz.consul.HealthClient;
+import com.orbitz.consul.async.ConsulResponseCallback;
+import com.orbitz.consul.model.health.HealthCheck;
+import com.orbitz.consul.model.health.ServiceHealth;
+import com.orbitz.consul.option.CatalogOptions;
+
+import java.math.BigInteger;
+import java.util.List;
+
+public class HealthCheckCache extends ConsulCache<String, HealthCheck> {
+
+    private HealthCheckCache(Function<HealthCheck, String> keyConversion, CallbackConsumer<HealthCheck> callbackConsumer) {
+        super(keyConversion, callbackConsumer);
+    }
+
+    /**
+     * Factory method to construct a string/{@link HealthCheck} map for a particular {@link com.orbitz.consul.model.State}.
+     * <p/>
+     * Keys will be the {@link HealthCheck#getCheckId()}.
+     *
+     * @param healthClient the {@link HealthClient}
+     * @param state        the state fo filter checks
+     * @return a cache object
+     */
+    public static HealthCheckCache newCache(
+            final HealthClient healthClient,
+            final com.orbitz.consul.model.State state,
+            final CatalogOptions catalogOptions,
+            final int watchSeconds) {
+        Function<HealthCheck, String> keyExtractor = new Function<HealthCheck, String>() {
+            @Override
+            public String apply(HealthCheck input) {
+                return input.getCheckId();
+            }
+        };
+
+        CallbackConsumer<HealthCheck> callbackConsumer = new CallbackConsumer<HealthCheck>() {
+            @Override
+            public void consume(BigInteger index, ConsulResponseCallback<List<HealthCheck>> callback) {
+                healthClient.getChecksByState(state, catalogOptions, watchParams(index, watchSeconds), callback);
+            }
+        };
+
+        return new HealthCheckCache(keyExtractor, callbackConsumer);
+
+    }
+
+    public static HealthCheckCache newCache(final HealthClient healthClient, final com.orbitz.consul.model.State state) {
+        return newCache(healthClient, state, CatalogOptions.BLANK, 10);
+    }
+
+}
