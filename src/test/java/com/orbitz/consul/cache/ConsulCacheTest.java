f6a2d1fbd37e6fbed518bf48c12977f3c1e1f109
esther <esther.vega@nexmo.com>
2016-08-10 10:21:54 +0100

    clean imports in tests class

--- src/test/java/com/orbitz/consul/cache/ConsulCacheTest.java
+++ src/test/java/com/orbitz/consul/cache/ConsulCacheTest.java
@@ -25,7 +25,12 @@ import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.fail;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 public class ConsulCacheTest extends BaseIntegrationTest {
 
