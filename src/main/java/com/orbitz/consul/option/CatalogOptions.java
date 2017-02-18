92d58653d4b9dc1c643d8eb9af8ba2c35ca6cb7d
rfast <rick.t.fast@gmail.com>
2017-02-18 14:02:42 -0600

    deprecate CatalogOptions

--- src/main/java/com/orbitz/consul/option/CatalogOptions.java
+++ src/main/java/com/orbitz/consul/option/CatalogOptions.java
@@ -4,15 +4,21 @@ import com.google.common.base.Optional;
 import org.immutables.value.Value;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static com.orbitz.consul.option.Options.optionallyAdd;
 
 @Value.Immutable
+@Deprecated
+/**
+ * @deprecated Will be rolled into {@link QueryOptions} in the next major
+ *  release.
+ */
 public abstract class CatalogOptions implements ParamAdder {
 
     public abstract Optional<String> getDatacenter();
-    public abstract Optional<String> getTag();
+    public abstract List<String> getTag();
 
     public static final CatalogOptions BLANK = ImmutableCatalogOptions.builder().build();
 
@@ -21,7 +27,6 @@ public abstract class CatalogOptions implements ParamAdder {
         Map<String, Object> result = new HashMap<>();
 
         optionallyAdd(result, "dc", getDatacenter());
-        optionallyAdd(result, "tag", getTag());
 
         return result;
     }
