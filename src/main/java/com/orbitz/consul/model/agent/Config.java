0f4ea7aa220289ad2a76512a5e6652ee45b90282
Furcube <furcube@gmail.com>
2017-11-01 14:48:05 +0300

    Compatibility with Consul 1.0 (#268)
    * Consul 1.0
    
    * compatibility with Consul older than 1.0

--- src/main/java/com/orbitz/consul/model/agent/Config.java
+++ src/main/java/com/orbitz/consul/model/agent/Config.java
@@ -16,97 +16,18 @@ import java.util.List;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public abstract class Config {
 
-    @JsonProperty("Bootstrap")
-    public abstract boolean getBootstrap();
-
-    @JsonProperty("Server")
-    public abstract boolean getServer();
-
     @JsonProperty("Datacenter")
     public abstract String getDatacenter();
-
-    @JsonProperty("DataDir")
-    public abstract String getDataDir();
-
-    @JsonProperty("DNSRecursor")
-    public abstract String dnsRecursor();
-
-    @JsonProperty("Domain")
-    public abstract String getDomain();
-
-    @JsonProperty("LogLevel")
-    public abstract String getLogLevel();
 
     @JsonProperty("NodeName")
     public abstract String getNodeName();
 
-    @JsonProperty("ClientAddr")
-    public abstract String getClientAddr();
+    @JsonProperty("Revision")
+    public abstract String getRevision();
 
-    @JsonProperty("BindAddr")
-    public abstract String getBindAddr();
+    @JsonProperty("Server")
+    public abstract boolean getServer();
 
-    @JsonProperty("AdvertiseAddr")
-    public abstract String getAdvertiseAddr();
-
-    @JsonProperty("Ports")
-    public abstract Ports getPorts();
-
-    @JsonProperty("LeaveOnTerm")
-    public abstract boolean getLeaveOnTerm();
-
-    @JsonProperty("SkipLeaveOnInt")
-    public abstract boolean getSkipLeaveOnInt();
-
-    /**
-     *
-     * @deprecated GET /v1/agent/self from v0.6.4 does not have this JSON field
-     */
-    @Deprecated
-    @JsonProperty("StatsiteAddr")
-    public abstract Optional<String> getStatsiteAddr();
-
-    @JsonProperty("Protocol")
-    public abstract int getProtocol();
-
-    @JsonProperty("EnableDebug")
-    public abstract boolean getEnableDebug();
-
-    @JsonProperty("VerifyIncoming")
-    public abstract boolean getVerifyIncoming();
-
-    @JsonProperty("VerifyOutgoing")
-    public abstract boolean getVerifyOutgoing();
-
-    @JsonProperty("CAFile")
-    public abstract String getCaFile();
-
-    @JsonProperty("CertFile")
-    public abstract String getCertFile();
-
-    @JsonProperty("KeyFile")
-    public abstract String getKeyFile();
-
-    @JsonProperty("StartJoin")
-    @JsonDeserialize(as = ImmutableList.class, contentAs = String.class)
-    public abstract List<String> getStartJoin();
-
-    @JsonProperty("UiDir")
-    public abstract Optional<String> getUiDir();
-
-    @JsonProperty("PidFile")
-    public abstract String getPidFile();
-
-    @JsonProperty("EnableSyslog")
-    public abstract boolean getEnableSyslog();
-
-    @JsonProperty("RejoinAfterLeave")
-    public abstract boolean getRejoinAfterLeave();
-
-    /**
-     * New version of consul has Telemetry field
-     * TODO: Have to think about back compatibility (I think we shouldn't)
-     */
-    @JsonProperty("Telemetry")
-    public abstract Optional<Telemetry> getTelemetry();
+    @JsonProperty("Version")
+    public abstract String getVersion();
 }
