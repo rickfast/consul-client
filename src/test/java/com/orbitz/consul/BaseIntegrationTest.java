2aa9c90f92d08efce0a6f481298a02c3e894c786
Yoann Fouquet <y.fouquet@criteo.com>
2017-12-27 11:57:22 +0100

    Decrease OkHTTP Read Timeout to 2 second
    Currently, the read timeout is the default value of okhttp i.e. 10 seconds.
    That is a very long duration when we consider testing.
    Decreasing it to 2 second should have limited to no impact.

--- src/test/java/com/orbitz/consul/BaseIntegrationTest.java
+++ src/test/java/com/orbitz/consul/BaseIntegrationTest.java
@@ -3,12 +3,17 @@ package com.orbitz.consul;
 import com.google.common.net.HostAndPort;
 import org.junit.BeforeClass;
 
+import java.time.Duration;
+
 public abstract class BaseIntegrationTest {
 
     protected static Consul client;
 
     @BeforeClass
     public static void beforeClass() {
-        client = Consul.builder().withHostAndPort(HostAndPort.fromParts("localhost", 8500)).build();
+        client = Consul.builder()
+                .withHostAndPort(HostAndPort.fromParts("localhost", 8500))
+                .withReadTimeoutMillis(Duration.ofSeconds(2).toMillis())
+                .build();
     }
 }
