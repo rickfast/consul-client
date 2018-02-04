673bf062f61a61d84b65788e4dfb9a6ace1a9aca
Yoann Fouquet <y.fouquet@criteo.com>
2018-02-04 14:15:43 +0100

    Inject the same Consul host/port everywhere for integration tests
    Most tests use the consul client created in BaseIntegrationTest.
    However, two classes do differently.
    If you do not use "localhost:8500" for your tests, you need to change it 5 times.
    Make every tests use the same client address.

--- src/test/java/com/orbitz/consul/util/CustomBuilderTest.java
+++ src/test/java/com/orbitz/consul/util/CustomBuilderTest.java
@@ -1,6 +1,5 @@
 package com.orbitz.consul.util;
 
-import com.google.common.net.HostAndPort;
 import com.orbitz.consul.BaseIntegrationTest;
 import com.orbitz.consul.Consul;
 import com.orbitz.consul.model.agent.Agent;
@@ -9,7 +8,6 @@ import org.junit.Test;
 import java.net.Proxy;
 import java.net.UnknownHostException;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class CustomBuilderTest extends BaseIntegrationTest{
@@ -17,7 +15,7 @@ public class CustomBuilderTest extends BaseIntegrationTest{
     @Test
     public void shouldConnectWithCustomTimeouts() throws UnknownHostException {
         Consul client = Consul.builder()
-                .withHostAndPort(HostAndPort.fromParts("localhost", 8500))
+                .withHostAndPort(defaultClientHostAndPort)
                 .withProxy(Proxy.NO_PROXY)
                 .withConnectTimeoutMillis(10000)
                 .withReadTimeoutMillis(3600000)
