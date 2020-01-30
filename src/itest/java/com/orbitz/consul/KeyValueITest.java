0c4594fc1e387793676783be1360486875485731
Chris Hansen <hansen.chris.w@gmail.com>
2020-01-30 13:19:54 -0700

    Add support for the separator param on getKeys (#408) (#410)
    * Return an empty list for a missing key from getKeys instead of throwing an exception (#225)
    
    * Add support for the separator param on getKeys (#408)

--- src/itest/java/com/orbitz/consul/KeyValueITest.java
+++ src/itest/java/com/orbitz/consul/KeyValueITest.java
@@ -477,8 +477,25 @@ public class KeyValueITest extends BaseIntegrationTest {
     }
 
     @Test
-    public void testUnknownKey() {
+    public void testGetKeysWithSeparator() {
+        KeyValueClient kvClient = client.keyValueClient();
+        kvClient.putValue("nested/first", "first");
+        kvClient.putValue("nested/second", "second");
+
+        List<String> keys = kvClient.getKeys("nested", "/");
+        assertEquals(1, keys.size());
+        assertEquals("nested/", keys.get(0));
+    }
+
+    @Test
+    public void testUnknownKeyGetValues() {
         List<String> shouldBeEmpty = client.keyValueClient().getValuesAsString("unknownKey");
+        assertTrue(shouldBeEmpty.isEmpty());
+    }
+
+    @Test
+    public void testUnknownKeyGetKeys() {
+        List<String> shouldBeEmpty = client.keyValueClient().getKeys("unknownKey");
         assertTrue(shouldBeEmpty.isEmpty());
     }
 }
