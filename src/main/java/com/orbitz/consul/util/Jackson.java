f77c1db74ceace1825d2a3375854f66694f7ba5b
Mike Pilone <mpilone@npr.org>
2020-02-13 15:35:16 -0500

    Added serialization inclusion of non-null, non-absent, and non-empty to work with Consul breaking API change in 1.7.0 via https://github.com/hashicorp/consul/pull/6874

--- src/main/java/com/orbitz/consul/util/Jackson.java
+++ src/main/java/com/orbitz/consul/util/Jackson.java
@@ -1,5 +1,6 @@
 package com.orbitz.consul.util;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.guava.GuavaModule;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
@@ -12,6 +13,9 @@ public class Jackson {
         ObjectMapper mapper = new ObjectMapper();
         mapper.registerModule(new Jdk8Module());
         mapper.registerModule(new GuavaModule());
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_ABSENT);
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
         return mapper;
     }
 
