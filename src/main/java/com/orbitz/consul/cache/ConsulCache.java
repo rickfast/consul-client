b3a21e881ece35f324e3895fd392beb074358401
Mahmoud Ashraf Algammal <engmahmoudalgamal@gmail.com>
2020-06-28 00:33:19 +0200

    Upgrade to guava.version 29.0-jre (#419)
    * upgrade to guava.version 29.0-jre
    
    public long elapsed() has got changed to  public long elapsed(TimeUnit desiredUnit) 
    which makes an exception Method not found if using this with consul-all
    
    * upgrade to guava.version29.0-jre Pass TimeUnit to stopWatch.elapsed(TimeUnit.MILLISECONDS)
    
    Duration elapsedTime = stopWatch.elapsed();
    long elapsedTime = stopWatch.elapsed(TimeUnit.MILLISECONDS);
    
    Co-authored-by: mahmoudashraf91 <39341898+mahmoudashraf91@users.noreply.github.com>

--- src/main/java/com/orbitz/consul/cache/ConsulCache.java
+++ src/main/java/com/orbitz/consul/cache/ConsulCache.java
@@ -122,10 +122,10 @@ public class ConsulCache<K, V> implements AutoCloseable {
                     if (!isRunning()) {
                         return;
                     }
-                    Duration elapsedTime = stopWatch.elapsed();
+                    long elapsedTime = stopWatch.elapsed(TimeUnit.MILLISECONDS);
                     updateIndex(consulResponse);
                     LOGGER.debug("Consul cache updated for {} (index={}), request duration: {} ms",
-                            cacheDescriptor, latestIndex, elapsedTime.toMillis());
+                            cacheDescriptor, latestIndex, elapsedTime);
 
                     ImmutableMap<K, V> full = convertToMap(consulResponse);
 
